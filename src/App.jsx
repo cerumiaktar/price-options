68244d80847abe2dc8b87b961e47b974f78724df
rumiaktar <ce.rumiakter@gmail.com>
2024-03-21 00:29:57 +0600

    add axios

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import LineChart from './components/LineChart/LineChart'
 // import DaisyNav from './components/DaisyNav/DaisyNav'
 import NavBar from './components/NavBar/NavBar'
+import Phones from './components/Phones/Phones'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <h1 className='text-4xl bg-rose-600'>Vite + React</h1>
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
+      <Phones></Phones>
     </>
   )
 }
